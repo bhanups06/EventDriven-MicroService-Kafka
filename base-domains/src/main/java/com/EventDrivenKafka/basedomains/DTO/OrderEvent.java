ed1100fef51f99c37e09f09acec3512c362b5d45
bhanu <pratapbhanu6@gmail.com>
2024-03-21 16:44:12 -0400

    This is the initial commit for event driven microservice kafka project

---
+++ base-domains/src/main/java/com/EventDrivenKafka/basedomains/DTO/OrderEvent.java
@@ -0,0 +1,39 @@
+package com.EventDrivenKafka.basedomains.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderEvent {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    private String status;
+    private Order order;
+}
---
+++ base-domains/src/test/java/com/EventDrivenKafka/basedomains/BaseDomainsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EventDrivenKafka.basedomains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseDomainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
